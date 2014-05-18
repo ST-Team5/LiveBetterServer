ea036f2569e60c582cf1d7974bec015364987bad
Stoyan Stoyanov <djstoxer@gmail.com>
2014-05-18 17:10:40 +0300

    Local changes are made. Service for adding new activity type is added.

--- live-better/src/main/java/com/livebetter/controllers/ActivityController.java
+++ live-better/src/main/java/com/livebetter/controllers/ActivityController.java
@@ -1,33 +1,41 @@
 package com.livebetter.controllers;
+import antlr.collections.List;
+
 import com.livebetter.domain.Activity;
 import com.livebetter.services.ActivityService;
+import com.livebetter.services.ActivityServiceImpl;
 import com.livebetter.services.PersonActivityService;
+
 import java.io.UnsupportedEncodingException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
-@RequestMapping("/activitieses")
 @Controller
 public class ActivityController {
 
 	@Autowired
-    ActivityService activityService;
+    ActivityService activityService = new ActivityServiceImpl();
 
 	@Autowired
     PersonActivityService personActivityService;
-
+	/*
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
     public String create(@Valid Activity activity, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
@@ -115,5 +123,17 @@ public class ActivityController {
             pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
         } catch (UnsupportedEncodingException uee) {}
         return pathSegment;
+    }
+	*/
+	
+	@RequestMapping(value = "activities/insert-new-type", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+    public @ResponseBody void InsertActivity(@RequestBody Activity activityAsJson) {
+
+		Activity act = new Activity();
+		act.setName(activityAsJson.getName());
+		act.setType(activityAsJson.getType());
+		act.setCaloriesPerHour(activityAsJson.getCaloriesPerHour());
+
+		activityService.saveActivities(act); 
     }
 }
