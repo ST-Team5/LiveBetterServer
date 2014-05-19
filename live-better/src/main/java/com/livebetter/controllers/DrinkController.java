bfb00665e831dce012548e68f2f4c5bdc832ef17
Georgi Hristozov <georgi@forkbomb.nl>
2014-05-19 08:40:01 +0300

    Take two.

--- live-better/src/main/java/com/livebetter/controllers/DrinkController.java
+++ live-better/src/main/java/com/livebetter/controllers/DrinkController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
-@RequestMapping("/drinkses")
 @Controller
 public class DrinkController {
 
@@ -26,95 +25,95 @@ public class DrinkController {
 
 	@Autowired
     PersonDrinkService personDrinkService;
-
-	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid Drink drink, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
-        if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, drink);
-            return "drinkses/create";
-        }
-        uiModel.asMap().clear();
-        drinkService.saveDrinks(drink);
-        return "redirect:/drinkses/" + encodeUrlPathSegment(drink.getId().toString(), httpServletRequest);
-    }
-
-	@RequestMapping(params = "form", produces = "text/html")
-    public String createForm(Model uiModel) {
-        populateEditForm(uiModel, new Drink());
-        return "drinkses/create";
-    }
-
-	@RequestMapping(value = "/{id}", produces = "text/html")
-    public String show(@PathVariable("id") Long id, Model uiModel) {
-        addDateTimeFormatPatterns(uiModel);
-        uiModel.addAttribute("drinks", drinkService.findDrinks(id));
-        uiModel.addAttribute("itemId", id);
-        return "drinkses/show";
-    }
-
-	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
-        if (page != null || size != null) {
-            int sizeNo = size == null ? 10 : size.intValue();
-            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-            uiModel.addAttribute("drinkses", Drink.findDrinksEntries(firstResult, sizeNo, sortFieldName, sortOrder));
-            float nrOfPages = (float) drinkService.countAllDrinkses() / sizeNo;
-            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
-        } else {
-            uiModel.addAttribute("drinkses", Drink.findAllDrinkses(sortFieldName, sortOrder));
-        }
-        addDateTimeFormatPatterns(uiModel);
-        return "drinkses/list";
-    }
-
-	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
-    public String update(@Valid Drink drink, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
-        if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, drink);
-            return "drinkses/update";
-        }
-        uiModel.asMap().clear();
-        drinkService.updateDrinks(drink);
-        return "redirect:/drinkses/" + encodeUrlPathSegment(drink.getId().toString(), httpServletRequest);
-    }
-
-	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
-    public String updateForm(@PathVariable("id") Long id, Model uiModel) {
-        populateEditForm(uiModel, drinkService.findDrinks(id));
-        return "drinkses/update";
-    }
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
-    public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
-        Drink drink = drinkService.findDrinks(id);
-        drinkService.deleteDrinks(drink);
-        uiModel.asMap().clear();
-        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
-        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
-        return "redirect:/drinkses";
-    }
-
-	void addDateTimeFormatPatterns(Model uiModel) {
-        uiModel.addAttribute("drinks_createddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
-        uiModel.addAttribute("drinks_modifieddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
-    }
-
-	void populateEditForm(Model uiModel, Drink drink) {
-        uiModel.addAttribute("drinks", drink);
-        addDateTimeFormatPatterns(uiModel);
-        uiModel.addAttribute("persondrinkses", personDrinkService.findAllPersonDrinkses());
-    }
-
-	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
-        String enc = httpServletRequest.getCharacterEncoding();
-        if (enc == null) {
-            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
-        }
-        try {
-            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-        } catch (UnsupportedEncodingException uee) {}
-        return pathSegment;
-    }
+//
+//	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
+//    public String create(@Valid Drink drink, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+//        if (bindingResult.hasErrors()) {
+//            populateEditForm(uiModel, drink);
+//            return "drinkses/create";
+//        }
+//        uiModel.asMap().clear();
+//        drinkService.saveDrinks(drink);
+//        return "redirect:/drinkses/" + encodeUrlPathSegment(drink.getId().toString(), httpServletRequest);
+//    }
+//
+//	@RequestMapping(params = "form", produces = "text/html")
+//    public String createForm(Model uiModel) {
+//        populateEditForm(uiModel, new Drink());
+//        return "drinkses/create";
+//    }
+//
+//	@RequestMapping(value = "/{id}", produces = "text/html")
+//    public String show(@PathVariable("id") Long id, Model uiModel) {
+//        addDateTimeFormatPatterns(uiModel);
+//        uiModel.addAttribute("drinks", drinkService.findDrinks(id));
+//        uiModel.addAttribute("itemId", id);
+//        return "drinkses/show";
+//    }
+//
+//	@RequestMapping(produces = "text/html")
+//    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+//        if (page != null || size != null) {
+//            int sizeNo = size == null ? 10 : size.intValue();
+//            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+//            uiModel.addAttribute("drinkses", Drink.findDrinksEntries(firstResult, sizeNo, sortFieldName, sortOrder));
+//            float nrOfPages = (float) drinkService.countAllDrinkses() / sizeNo;
+//            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+//        } else {
+//            uiModel.addAttribute("drinkses", Drink.findAllDrinkses(sortFieldName, sortOrder));
+//        }
+//        addDateTimeFormatPatterns(uiModel);
+//        return "drinkses/list";
+//    }
+//
+//	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
+//    public String update(@Valid Drink drink, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+//        if (bindingResult.hasErrors()) {
+//            populateEditForm(uiModel, drink);
+//            return "drinkses/update";
+//        }
+//        uiModel.asMap().clear();
+//        drinkService.updateDrinks(drink);
+//        return "redirect:/drinkses/" + encodeUrlPathSegment(drink.getId().toString(), httpServletRequest);
+//    }
+//
+//	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
+//    public String updateForm(@PathVariable("id") Long id, Model uiModel) {
+//        populateEditForm(uiModel, drinkService.findDrinks(id));
+//        return "drinkses/update";
+//    }
+//
+//	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
+//    public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+//        Drink drink = drinkService.findDrinks(id);
+//        drinkService.deleteDrinks(drink);
+//        uiModel.asMap().clear();
+//        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
+//        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
+//        return "redirect:/drinkses";
+//    }
+//
+//	void addDateTimeFormatPatterns(Model uiModel) {
+//        uiModel.addAttribute("drinks_createddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
+//        uiModel.addAttribute("drinks_modifieddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
+//    }
+//
+//	void populateEditForm(Model uiModel, Drink drink) {
+//        uiModel.addAttribute("drinks", drink);
+//        addDateTimeFormatPatterns(uiModel);
+//        uiModel.addAttribute("persondrinkses", personDrinkService.findAllPersonDrinkses());
+//    }
+//
+//	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
+//        String enc = httpServletRequest.getCharacterEncoding();
+//        if (enc == null) {
+//            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
+//        }
+//        try {
+//            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
+//        } catch (UnsupportedEncodingException uee) {}
+//        return pathSegment;
+//    }
 
     @RequestMapping(value = "drinks/insert", method = RequestMethod.POST, headers = {"Content-type=application/json"})
     public @ResponseBody
--- live-better/src/main/java/com/livebetter/controllers/MealController.java
+++ live-better/src/main/java/com/livebetter/controllers/MealController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
-@RequestMapping("/mealses")
 @Controller
 public class MealController {
 
@@ -27,94 +26,94 @@ public class MealController {
 	@Autowired
     PersonMealService personMealService;
 
-	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid Meal meal, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
-        if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, meal);
-            return "mealses/create";
-        }
-        uiModel.asMap().clear();
-        mealService.saveMeals(meal);
-        return "redirect:/mealses/" + encodeUrlPathSegment(meal.getId().toString(), httpServletRequest);
-    }
-
-	@RequestMapping(params = "form", produces = "text/html")
-    public String createForm(Model uiModel) {
-        populateEditForm(uiModel, new Meal());
-        return "mealses/create";
-    }
-
-	@RequestMapping(value = "/{id}", produces = "text/html")
-    public String show(@PathVariable("id") Long id, Model uiModel) {
-        addDateTimeFormatPatterns(uiModel);
-        uiModel.addAttribute("meals", mealService.findMeals(id));
-        uiModel.addAttribute("itemId", id);
-        return "mealses/show";
-    }
-
-	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
-        if (page != null || size != null) {
-            int sizeNo = size == null ? 10 : size.intValue();
-            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-            uiModel.addAttribute("mealses", Meal.findMealsEntries(firstResult, sizeNo, sortFieldName, sortOrder));
-            float nrOfPages = (float) mealService.countAllMealses() / sizeNo;
-            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
-        } else {
-            uiModel.addAttribute("mealses", Meal.findAllMealses(sortFieldName, sortOrder));
-        }
-        addDateTimeFormatPatterns(uiModel);
-        return "mealses/list";
-    }
-
-	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
-    public String update(@Valid Meal meal, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
-        if (bindingResult.hasErrors()) {
-            populateEditForm(uiModel, meal);
-            return "mealses/update";
-        }
-        uiModel.asMap().clear();
-        mealService.updateMeals(meal);
-        return "redirect:/mealses/" + encodeUrlPathSegment(meal.getId().toString(), httpServletRequest);
-    }
-
-	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
-    public String updateForm(@PathVariable("id") Long id, Model uiModel) {
-        populateEditForm(uiModel, mealService.findMeals(id));
-        return "mealses/update";
-    }
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
-    public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
-        Meal meal = mealService.findMeals(id);
-        mealService.deleteMeals(meal);
-        uiModel.asMap().clear();
-        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
-        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
-        return "redirect:/mealses";
-    }
-
-	void addDateTimeFormatPatterns(Model uiModel) {
-        uiModel.addAttribute("meals_createddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
-        uiModel.addAttribute("meals_modifieddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
-    }
-
-	void populateEditForm(Model uiModel, Meal meal) {
-        uiModel.addAttribute("meals", meal);
-        addDateTimeFormatPatterns(uiModel);
-        uiModel.addAttribute("personmealses", personMealService.findAllPersonMealses());
-    }
-
-	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
-        String enc = httpServletRequest.getCharacterEncoding();
-        if (enc == null) {
-            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
-        }
-        try {
-            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-        } catch (UnsupportedEncodingException uee) {}
-        return pathSegment;
-    }
+//	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
+//    public String create(@Valid Meal meal, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+//        if (bindingResult.hasErrors()) {
+//            populateEditForm(uiModel, meal);
+//            return "mealses/create";
+//        }
+//        uiModel.asMap().clear();
+//        mealService.saveMeals(meal);
+//        return "redirect:/mealses/" + encodeUrlPathSegment(meal.getId().toString(), httpServletRequest);
+//    }
+//
+//	@RequestMapping(params = "form", produces = "text/html")
+//    public String createForm(Model uiModel) {
+//        populateEditForm(uiModel, new Meal());
+//        return "mealses/create";
+//    }
+//
+//	@RequestMapping(value = "/{id}", produces = "text/html")
+//    public String show(@PathVariable("id") Long id, Model uiModel) {
+//        addDateTimeFormatPatterns(uiModel);
+//        uiModel.addAttribute("meals", mealService.findMeals(id));
+//        uiModel.addAttribute("itemId", id);
+//        return "mealses/show";
+//    }
+//
+//	@RequestMapping(produces = "text/html")
+//    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+//        if (page != null || size != null) {
+//            int sizeNo = size == null ? 10 : size.intValue();
+//            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+//            uiModel.addAttribute("mealses", Meal.findMealsEntries(firstResult, sizeNo, sortFieldName, sortOrder));
+//            float nrOfPages = (float) mealService.countAllMealses() / sizeNo;
+//            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+//        } else {
+//            uiModel.addAttribute("mealses", Meal.findAllMealses(sortFieldName, sortOrder));
+//        }
+//        addDateTimeFormatPatterns(uiModel);
+//        return "mealses/list";
+//    }
+//
+//	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
+//    public String update(@Valid Meal meal, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+//        if (bindingResult.hasErrors()) {
+//            populateEditForm(uiModel, meal);
+//            return "mealses/update";
+//        }
+//        uiModel.asMap().clear();
+//        mealService.updateMeals(meal);
+//        return "redirect:/mealses/" + encodeUrlPathSegment(meal.getId().toString(), httpServletRequest);
+//    }
+//
+//	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
+//    public String updateForm(@PathVariable("id") Long id, Model uiModel) {
+//        populateEditForm(uiModel, mealService.findMeals(id));
+//        return "mealses/update";
+//    }
+//
+//	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
+//    public String delete(@PathVariable("id") Long id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+//        Meal meal = mealService.findMeals(id);
+//        mealService.deleteMeals(meal);
+//        uiModel.asMap().clear();
+//        uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
+//        uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
+//        return "redirect:/mealses";
+//    }
+//
+//	void addDateTimeFormatPatterns(Model uiModel) {
+//        uiModel.addAttribute("meals_createddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
+//        uiModel.addAttribute("meals_modifieddatetime_date_format", DateTimeFormat.patternForStyle("MM", LocaleContextHolder.getLocale()));
+//    }
+//
+//	void populateEditForm(Model uiModel, Meal meal) {
+//        uiModel.addAttribute("meals", meal);
+//        addDateTimeFormatPatterns(uiModel);
+//        uiModel.addAttribute("personmealses", personMealService.findAllPersonMealses());
+//    }
+//
+//	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
+//        String enc = httpServletRequest.getCharacterEncoding();
+//        if (enc == null) {
+//            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
+//        }
+//        try {
+//            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
+//        } catch (UnsupportedEncodingException uee) {}
+//        return pathSegment;
+//    }
 
     @RequestMapping(value = "meals/insert", method = RequestMethod.POST, headers = {"Content-type=application/json"})
     public @ResponseBody
